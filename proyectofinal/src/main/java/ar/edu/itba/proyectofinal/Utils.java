cdda58a5e3346df795c210a5dbb5e86eabc3ee25
maggievega <maggievega@hotmail.com>
2018-05-14 01:10:45 -0300

    Added class input so that we can read the data, started analizing verlet

--- proyectofinal/src/main/java/ar/edu/itba/proyectofinal/Utils.java
+++ proyectofinal/src/main/java/ar/edu/itba/proyectofinal/Utils.java
@@ -13,10 +13,10 @@ public class Utils {
     public static double getAngle (Point massCenter, Point desired) {
         double x1 = 1;
         double y1 = 0;
-        double x2 = desired.getX()-massCenter.getX();
-        double y2 = desired.getY()-massCenter.getY();
+        double x2 = desired.getX() - massCenter.getX();
+        double y2 = desired.getY() - massCenter.getY();
         double value = Math.atan2(x1 * y2 - y1 * x2, x1 * x2 + y1 * y2);
-        return value >= 0 ? value : value + 2*Math.PI;
+        return value >= 0 ? value : value + 2 * Math.PI;
     }
 
 
